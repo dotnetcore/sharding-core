ad7ad31ffe1ad4bafd0fdf99a2ca8e66f166a096
xjm <326308290@qq.com>
2021-02-02 21:50:22 +0800

    1

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,8 +1,8 @@
 :start
 ::定义版本
-set EFCORE2=2.0.0.2
-set EFCORE3=3.0.0.2
-set EFCORE5=5.0.0.2
+set EFCORE2=2.0.0.3
+set EFCORE3=3.0.0.3
+set EFCORE5=5.0.0.3
 
 ::删除所有bin与obj下的文件
 @echo off
@@ -22,12 +22,12 @@ for /r %nowpath% %%i in (*.pdb,*.vshost.*) do (del %%i && echo delete %%i)
 ::delete specify folder(obj,bin)
 for /r %nowpath% %%i in (obj,bin) do (IF EXIST %%i (RD /s /q %%i && echo delete %%i))
 
-echo 清理完成
+echo clear complete
 
 ::构建
 dotnet build -c Release
-::推送
+::publishing....
 for /r %nowpath% %%i in (*.nupkg) do (dotnet nuget push %%i -k %apiKey% --source https://api.nuget.org/v3/index.json)
 
-echo 完成
+echo complete
 pause
