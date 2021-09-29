85f854577de59c2ed996d7f942498f4717678236
xuejiaming <326308290@qq.com>
2021-09-29 14:42:50 +0800

    efcore2下修复iasyncenumerable的current报错问题

--- src/ShardingCore/Sharding/Enumerators/StreamMergeAsync/StreamMergeAsyncEnumerator.cs
+++ src/ShardingCore/Sharding/Enumerators/StreamMergeAsync/StreamMergeAsyncEnumerator.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace ShardingCore.Sharding.Enumerators
 {
-    /*
-    * @Author: xjm
-    * @Description:
-    * @Date: Saturday, 14 August 2021 21:25:50
-    * @Email: 326308290@qq.com
-    */
     public class StreamMergeAsyncEnumerator<T> : IStreamMergeAsyncEnumerator<T>
     {
         private readonly IAsyncEnumerator<T> _asyncSource;
@@ -22,6 +16,7 @@ namespace ShardingCore.Sharding.Enumerators
         {
             if (_syncSource != null)
                 throw new ArgumentNullException(nameof(_syncSource));
+
             _asyncSource = asyncSource;
             skip = true;
         }
@@ -60,18 +55,38 @@ namespace ShardingCore.Sharding.Enumerators
             return await _asyncSource.MoveNextAsync();
         }
 
-        public T Current => GetCurrent();
-        public T ReallyCurrent => GetReallyCurrent();
+        public void Dispose()
+        {
+            _syncSource?.Dispose();
+        }
+
+#endif
+        public bool MoveNext()
+        {
+            if (skip)
+            {
+                skip = false;
+                return null != _syncSource.Current;
+            }
+            return _syncSource.MoveNext();
+        }
+
         public bool HasElement()
         {
             if (_asyncSource != null) return null != _asyncSource.Current;
             if (_syncSource != null) return null != _syncSource.Current;
             return false;
         }
-        public void Dispose()
+
+
+        public void Reset()
         {
-            _syncSource?.Dispose();
+            throw new NotImplementedException();
         }
+
+        object IEnumerator.Current => Current;
+        public T Current => GetCurrent();
+        public T ReallyCurrent => GetReallyCurrent();
         public T GetCurrent()
         {
             if (skip)
@@ -86,26 +101,6 @@ namespace ShardingCore.Sharding.Enumerators
             if (_syncSource != null) return _syncSource.Current;
             return default;
         }
-        public bool MoveNext()
-        {
-            if (skip)
-            {
-                skip = false;
-                return null != _syncSource.Current;
-            }
-            return _syncSource.MoveNext();
-        }
-
-#endif
-
-
-
-        public void Reset()
-        {
-            throw new NotImplementedException();
-        }
-
-        object IEnumerator.Current => Current;
 #if EFCORE2
         public void Dispose()
         {
@@ -118,61 +113,11 @@ namespace ShardingCore.Sharding.Enumerators
             if (skip)
             {
                 skip = false;
-                return null != SourceCurrent();
+                return null != _asyncSource.Current;
             }
             return await _asyncSource.MoveNext(cancellationToken);
         }
-        public T Current => GetCurrent();
-        public T ReallyCurrent => GetReallyCurrent();
-        public bool HasElement()
-        {
-            return null != SourceCurrent();
-        }
 
-        private T SourceCurrent()
-        {
-            try
-            {
-                if (tryGetCurrentError)
-                    return default;
-                if (_asyncSource!= null)
-                    return _asyncSource.Current;
-                if (_syncSource != null)
-                    return _syncSource.Current;
-                return default;
-            }
-            catch (Exception e)
-            {
-                tryGetCurrentError = true;
-                return default;
-            }
-        }
-
-        private bool tryGetCurrentError = false;
-
-        public T GetCurrent()
-        {
-            if (skip)
-                return default;
-            if (_asyncSource != null) return SourceCurrent();
-            if (_syncSource != null) return _syncSource.Current;
-            return default;
-        }
-        public T GetReallyCurrent()
-        {
-            if (_asyncSource != null) return SourceCurrent();
-            if (_syncSource != null) return _syncSource.Current;
-            return default;
-        }
-        public bool MoveNext()
-        {
-            if (skip)
-            {
-                skip = false;
-                return null != _syncSource.Current;
-            }
-            return _syncSource.MoveNext();
-        }
 
 #endif
     }
