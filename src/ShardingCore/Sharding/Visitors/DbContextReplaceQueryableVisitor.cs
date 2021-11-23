b64829636bb5208acfbc6dc311c82c592f0560b3
xuejiaming <326308290@qq.com>
2021-11-23 14:01:57 +0800

    优化代码发布版本x.3.1.48

--- src/ShardingCore/Sharding/Visitors/DbContextReplaceQueryableVisitor.cs
+++ src/ShardingCore/Sharding/Visitors/DbContextReplaceQueryableVisitor.cs
@@ -32,7 +32,8 @@ namespace ShardingCore.Core.Internal.Visitors
             {
                 var dbContextDependencies = typeof(DbContext).GetTypePropertyValue(_dbContext, "DbContextDependencies") as IDbContextDependencies;
                 var targetIQ = (IQueryable)((IDbSetCache)_dbContext).GetOrAddSet(dbContextDependencies.SetSource, queryable.ElementType);
-                var newQueryable = targetIQ.Provider.CreateQuery((Expression) Expression.Call((Expression) null, typeof(EntityFrameworkQueryableExtensions).GetTypeInfo().GetDeclaredMethod("AsNoTracking").MakeGenericMethod(queryable.ElementType), targetIQ.Expression));
+                //var newQueryable = targetIQ.Provider.CreateQuery((Expression) Expression.Call((Expression) null, typeof(EntityFrameworkQueryableExtensions).GetTypeInfo().GetDeclaredMethod("AsNoTracking").MakeGenericMethod(queryable.ElementType), targetIQ.Expression));
+                var newQueryable = targetIQ.Provider.CreateQuery(targetIQ.Expression);
                 Source = newQueryable;
 // return base.Visit(Expression.Constant(newQueryable));
                  return Expression.Constant(newQueryable);
