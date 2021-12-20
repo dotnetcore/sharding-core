e029a017b7fdd7625147d2c5e46ff93ae4834947
xuejiaming <326308290@qq.com>
2021-12-20 14:06:39 +0800

    发布x.3.1.91

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,9 +1,9 @@
 :start
 ::定义版本
-set EFCORE2=2.3.1.90
-set EFCORE3=3.3.1.90
-set EFCORE5=5.3.1.90
-set EFCORE6=6.3.1.90
+set EFCORE2=2.3.1.91
+set EFCORE3=3.3.1.91
+set EFCORE5=5.3.1.91
+set EFCORE6=6.3.1.91
 
 ::删除所有bin与obj下的文件
 @echo off
