5d3aa6dbc27d5ae50d54591ab5c5c7e90d436d55
xuejiaming <326308290@qq.com>
2022-05-28 23:23:50 +0800

    发布x.4.3.4

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,9 +1,9 @@
 :start
 ::定义版本
-set EFCORE2=2.4.3.3
-set EFCORE3=3.4.3.3
-set EFCORE5=5.4.3.3
-set EFCORE6=6.4.3.3
+set EFCORE2=2.4.3.4
+set EFCORE3=3.4.3.4
+set EFCORE5=5.4.3.4
+set EFCORE6=6.4.3.4
 
 ::删除所有bin与obj下的文件
 @echo off
