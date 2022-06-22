cbd55c928015af149ea9fc787c4bbb89956f8b9f
xuejiaming <326308290@qq.com>
2022-06-22 09:07:33 +0800

    创建为直接回收的dbcontext

--- src/ShardingCore/DynamicDataSources/DefaultDataSourceInitializer.cs
+++ src/ShardingCore/DynamicDataSources/DefaultDataSourceInitializer.cs
@@ -167,7 +167,7 @@ namespace ShardingCore.DynamicDataSources
             if (context is IShardingDbContext shardingDbContext)
             {
                 using (var dbContext =
-                       shardingDbContext.GetDbContext(dataSourceName, false,
+                       shardingDbContext.GetDbContext(dataSourceName, true,
                            _routeTailFactory.Create(string.Empty, false)))
                 {
                     var isDefault = virtualDataSource.IsDefault(dataSourceName);
@@ -190,7 +190,7 @@ namespace ShardingCore.DynamicDataSources
         {
             if (context is IShardingDbContext shardingDbContext)
             {
-                using (var dbContext = shardingDbContext.GetDbContext(dataSourceName, false,
+                using (var dbContext = shardingDbContext.GetDbContext(dataSourceName, true,
                            _routeTailFactory.Create(string.Empty, false)))
                 {
                     dbContext.RemoveDbContextAllRelationModel();
