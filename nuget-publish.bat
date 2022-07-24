8e5e658dc3a21fa52f3c3612e823195937278e3a
xuejiaming <326308290@qq.com>
2022-07-24 09:53:54 +0800

    发布x.6.0.19支持自定义builder

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,9 +1,9 @@
 :start
 ::定义版本
-set EFCORE2=2.6.0.18
-set EFCORE3=3.6.0.18
-set EFCORE5=5.6.0.18
-set EFCORE6=6.6.0.18
+set EFCORE2=2.6.0.19
+set EFCORE3=3.6.0.19
+set EFCORE5=5.6.0.19
+set EFCORE6=6.6.0.19
 
 ::删除所有bin与obj下的文件
 @echo off
