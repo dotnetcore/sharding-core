d3f346f433ce4fb5204d83f897792efe7f051c39
MysticBoy <mysticboy@live.com>
2022-09-28 16:49:57 +0800

    实现 PostgreSQL和 Sqlite 表是否存在的判断 接口

---
+++ src/ShardingCore/TableExists/SqliteTableEnsureManager.cs
@@ -0,0 +1,33 @@
+﻿using ShardingCore.Core.VirtualRoutes.TableRoutes.RouteTails.Abstractions;
+using ShardingCore.TableExists.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace ShardingCore.TableExists
+{
+    public class SqliteTableEnsureManager : AbstractTableEnsureManager
+    {
+        public SqliteTableEnsureManager(IRouteTailFactory routeTailFactory) : base(routeTailFactory)
+        {
+        }
+
+        public override ISet<string> DoGetExistTables(DbConnection connection, string dataSourceName)
+        {
+            ISet<string> result = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            using (var query = connection.CreateCommand())
+            {
+                query.CommandText = "SELECT tbl_name FROM  sqlite_master;";
+                using (var reader = query.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var str = (string)reader["tbl_name"];
+                        result.Add(str);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
