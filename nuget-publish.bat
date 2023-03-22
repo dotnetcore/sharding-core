eebbbef46dd39d0d2804b279a4b3b2c47e13c51e
xuejiaming <326308290@qq.com>
2023-03-22 11:16:50 +0800

    7.x.1.4修复视图时无法初始化

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,10 +1,10 @@
 :start
 ::定义版本
-set EFCORE7=7.7.1.3
-set EFCORE6=7.6.1.3
-set EFCORE5=7.5.1.3
-set EFCORE3=7.3.1.3
-set EFCORE2=7.2.1.3
+set EFCORE7=7.7.1.4
+set EFCORE6=7.6.1.4
+set EFCORE5=7.5.1.4
+set EFCORE3=7.3.1.4
+set EFCORE2=7.2.1.4
 
 ::删除所有bin与obj下的文件
 @echo off
