dc5fb1a487635294f35dd952764ffaf6ec423b6c
xuejiaming <326308290@qq.com>
2023-04-27 17:19:45 +0800

    还是需要聚合

--- src/ShardingCore/Sharding/MergeEngines/ShardingExecutors/ShardingExecutor.cs
+++ src/ShardingCore/Sharding/MergeEngines/ShardingExecutors/ShardingExecutor.cs
@@ -34,10 +34,10 @@ namespace ShardingCore.Sharding.MergeEngines.ShardingExecutors
             if (results.IsEmpty())
                 throw new ShardingCoreException("sharding execute result empty");
             //不需要merge
-            if (results.Count == 1)
-            {
-                return results[0];
-            }
+            //if (results.Count == 1)
+            //{
+            //    return results[0];
+            //}
             var streamMerge = executor.GetShardingMerger().StreamMerge(results);
             return streamMerge;
         }
