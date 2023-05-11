4b5b987fdc1541a6be5df5933ff2f428366f33f9
xuejiaming <326308290@qq.com>
2023-05-11 12:26:36 +0800

    7.x.1.9

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,10 +1,10 @@
 :start
 ::定义版本
-set EFCORE7=7.7.1.8
-set EFCORE6=7.6.1.8
-set EFCORE5=7.5.1.8
-set EFCORE3=7.3.1.8
-set EFCORE2=7.2.1.8
+set EFCORE7=7.7.1.9
+set EFCORE6=7.6.1.9
+set EFCORE5=7.5.1.9
+set EFCORE3=7.3.1.9
+set EFCORE2=7.2.1.9
 
 ::删除所有bin与obj下的文件
 @echo off
